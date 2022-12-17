3cacb48967737685c94b2d9924c1c6f5ae8b69c3
Sergii <sonik2552@gmail.com>
2022-12-17 17:40:30 +0100

    iteration5

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,9 +1,18 @@
- openMenuBtn.addEventListener('click', toggleMenu);
+(() => {
+  const menuContainer = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => menuContainer.classList.toggle('is-open');
+  openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
-  // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
+  window
+    .matchMedia('(min-width: 600px)')
+    .addEventListener(
+      'change',
+      e => e.matches && menuContainer.classList.remove('is-open'),
+    );
+})();
